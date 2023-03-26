cf16f44492a432b1fd981ae2cd1b912e4657c94d
swati <s.swati.5293@gmail.com>
2023-03-26 17:54:17 +0530

    26th March'23

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path'); //inbuilt 
 const port = 8000; //port number
-// const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 
 const app = express(); //creating express server
@@ -10,9 +10,9 @@ const app = express(); //creating express server
 app.use(express.urlencoded());//middleware which is required to process that req data coming in(which is usally in form of a string) into for ex. key-value pair
 app.use(cookieParser());
 
-// app.use(expressLayouts);
-// app.set('layout extractStyles', true);
-// app.set('layout extractScripts', true);
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // app.use('/', require('./routes'));
 
